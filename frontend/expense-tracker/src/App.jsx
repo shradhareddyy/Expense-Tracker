3714d99a98e2cc0a21288226eb4e64a5b9c28bce
shradhareddyy <prods01@pfw.edu>
2025-08-30 10:02:48 -0700

    Initial commit

---
+++ frontend/expense-tracker/src/App.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import { BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+ } from 'react-router-dom'
+
+ import Login from "./pages/Auth/Login";
+ import SignUp from './pages/Auth/SignUp';
+ import Home from './pages/Dashboard/Home';
+ import Income from './pages/Dashboard/Income';
+ import Expense from './pages/Dashboard/Expense';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Root/>} />
+        <Route path='/login' exact element={<Login />} />
+        <Route path='/signup' exact element={<SignUp />} />
+        <Route path='/dashboard' exact element={<Home />} />
+        <Route path='/expense' exact element={<Expense />} />
+        <Route path='/income' exact element={<Income />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
+
+const Root=()=>{
+  //CHECKING IF TOKEN EXSISTS IN LOCAL STORAGE
+  const isAuthenticated=!!localStorage.getItem("token");
+
+  //redirected to dashboard if authenticated,otherwise to login 
+  return isAuthenticated ? (<Navigate to="/dashboard"/>):(<Navigate to="/login"/>);
+};
---
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Income = () => {
+  return (
+    <div>income</div>
+  )
+}
+
+export default Income
